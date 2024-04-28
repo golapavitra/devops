69225f7131807d9601a1e3f5bd74ba9372916c0b
Pavitra <golapavitra@gmail.com>
2024-04-28 10:14:52 +0530

    Initail feature1 commit

---
+++ src/main/java/com/sample/devops/HomeController.java
@@ -0,0 +1,12 @@
+package com.sample.devops;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	 @GetMapping("/")
+	    public String home() {
+	        return "index"; // This corresponds to the name of the HTML file (without the extension)
+	    }
+
+}
